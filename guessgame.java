38d43f98d245a0de9987d332cb0971a72529d164
Durgaprasad003 <129769654+Durgaprasad003@users.noreply.github.com>
2023-04-04 22:55:34 +0530

    Add files via upload

---
+++ guessgame.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Random;
+class guess{
+    public void random()
+    {
+        System.out.println("__WELCOME TO GUESSING GAME__\n\n");
+        Random sc=new Random();
+
+        int random=sc.nextInt(100);
+        System.out.println("The guess should between 0 and 100");
+        System.out.println("In this game you will have five trials\n");
+        
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your guess number");
+        for(int i=0;i<5;i++)
+        {
+            int guess=input.nextInt();
+            if(random==guess)
+            {
+                System.out.println("CONGRATULATIONS");
+                
+            }
+            else if(random>guess)
+            {
+                System.out.println("You entered too low number");
+            }
+            else if(random<guess)
+            {
+                System.out.println("You entered high number");
+            }
+           
+
+        }
+        System.out.println("You lose");
+        System.out.println("Trials are ended");
+    }
+}
+public class guessgame
+{
+    public static void main(String[] args) {
+        guess var=new guess();
+        var.random();
+    }
+}
